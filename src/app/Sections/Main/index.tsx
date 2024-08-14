904247ca7e3b7f353b28ca87a50d83f11459c054
Abdullah Khalil <abdullah.khalil123321@gmail.com>
2024-08-14 13:47:19 -0700

    test

--- src/app/Sections/Main/index.tsx
+++ src/app/Sections/Main/index.tsx
@@ -1,38 +1,41 @@
-import React from "react";
-import style from "./main.module.css";
-import Button from "@/assets/button";
-import image from "@/public/creative-minimalist-letter-ak-logo-600nw-1501334273.webp";
-import Image from "next/image";
+import React from 'react'
+import style from './main.module.css'
+import Button from '@/assets/button'
+import image from '@/public/creative-minimalist-letter-ak-logo-600nw-1501334273.webp'
+import Image from 'next/image'
+import Link from 'next/link'
 const Home = () => {
   return (
     <div className={style.home}>
       <div className={style.profileHello}>
         <div className={style.profileImageHolder}>
-          <Image src={image} alt={"profile"} fill />
+          <Image src={image} alt={'profile'} fill />
         </div>
         <h1>Hello! I&apos;m Abdullah Khalil</h1>
       </div>
       <div className={style.talkSeeWork}>
         <h2>A Web Developer based in Islamabad, Pakistan</h2>
-        <p style={{ marginBottom: "30px" }}>
+        <p style={{ marginBottom: '30px' }}>
           Passionate creating great experience Digital Product
         </p>
 
         <div>
-          <Button text={"Talk with me"} />
-          <Button
-            text={"See my work"}
-            styles={{
-              backgroundColor: "white",
-              color: "black",
-              border: "var(--primary-background) 1px solid",
-              marginLeft: "20px",
-            }}
-          />
+          <Button text={'Talk with me'} />
+          <Link href="/archive">
+            <Button
+              text={'See my work'}
+              styles={{
+                backgroundColor: 'white',
+                color: 'black',
+                border: 'var(--primary-background) 1px solid',
+                marginLeft: '20px',
+              }}
+            />
+          </Link>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
