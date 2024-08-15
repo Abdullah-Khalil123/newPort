5a8f56554e39b448bc693aa5337773b6a074f4ca
Abdullah Khalil <abdullah.khalil123321@gmail.com>
2024-08-15 13:11:21 -0700

    contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 import style from './contact.module.css'
+import Image from 'next/image'
+import github from '@/public/github-svgrepo-com.svg'
+import linkedIn from '@/public/linkedin-svgrepo-com.svg'
+import insta from '@/public/insta-svgrepo-com.svg'
+import { link } from 'fs'
+import Button from '@/assets/button'
 
 const ContactMe = () => {
   return (
-    <div>
+    <div className={style.ContactMe}>
       <div className={style.getInTouch}>
         <h3>Get in touch</h3>
 
@@ -23,14 +29,24 @@ const ContactMe = () => {
         <section>
           <h4>Call me</h4>
           <p>Mon-Sat from 10am to 6pm</p>
-          <a href="">03123456789</a>
+          <a href="" id={style.num}>
+            03123456789
+          </a>
         </section>
 
         <div>
           <h4>Social media</h4>
-          <div className={style.ImageHolder}></div>
-          <div className={style.ImageHolder}></div>
-          <div className={style.ImageHolder}></div>
+          <div className={style.ImagesSocials}>
+            <div className={style.ImageHolder}>
+              <Image src={insta} alt={''} fill />
+            </div>
+            <div className={style.ImageHolder}>
+              <Image src={github} alt={''} fill />
+            </div>
+            <div className={style.ImageHolder}>
+              <Image src={linkedIn} alt={''} fill />
+            </div>
+          </div>
         </div>
       </div>
       <Forms />
@@ -39,7 +55,61 @@ const ContactMe = () => {
 }
 
 const Forms = () => {
-  return <div className={style.form}></div>
+  return (
+    <form action="" className={style.forms}>
+      <div className="flexItems">
+        <Input
+          title={'First Name'}
+          placeholder={'Alex'}
+          styles={{ marginRight: '20px', width: '100%' }}
+        />
+        <Input
+          title={'Last Name'}
+          placeholder={'Regan'}
+          styles={{ width: '100%' }}
+        />
+      </div>
+      <Input title={'Company Name'} placeholder={'Google'} />
+      <Input title={'Email'} placeholder={'abc@email.com'} />
+      <div className={style.input} style={{}}>
+        <p>Message</p>
+        <textarea
+          name="message"
+          placeholder="Tell us what i can help you with"
+        ></textarea>
+      </div>
+      <div className={style.receiveInfo}>
+        <input type="checkbox" />
+        <p>
+          I{"'"}d like to receive more information about this company. I
+          understand and agree to the Privacy Policy
+        </p>
+      </div>
+      <Button
+        text={'Send Message'}
+        styles={{ width: '100%', backgroundColor: '#6f86d6', height: '40px' }}
+      />
+    </form>
+  )
+}
+
+const Input = ({
+  title,
+  placeholder,
+  styles,
+  type = 'text',
+}: {
+  title: string
+  placeholder: string
+  styles?: CSSProperties
+  type?: string
+}) => {
+  return (
+    <div className={style.input} style={styles}>
+      <p>{title}</p>
+      <input type={type} placeholder={placeholder} />
+    </div>
+  )
 }
 
 export default ContactMe
--- src/assets/Toggle/index.tsx
+++ src/assets/Toggle/index.tsx
@@ -1,39 +1,51 @@
-"use client";
-import { CSSProperties, RefObject, useRef, useState } from "react";
-import style from "./toggle.module.css";
-import gsap from "gsap";
+'use client'
+import { CSSProperties, RefObject, useRef, useState } from 'react'
+import style from './toggle.module.css'
+import gsap from 'gsap'
 
 function handleClick(ref: RefObject<HTMLDivElement>, isActive: boolean) {
   if (isActive) {
-    gsap.to(ref.current, { x: 24, ease: "bounce.out" });
+    gsap.to(ref.current, { x: 24, ease: 'bounce.out' })
   } else {
-    gsap.to(ref.current, { x: 0, ease: "bounce.out" });
+    gsap.to(ref.current, { x: 0, ease: 'bounce.out' })
   }
 }
 
 function handleTheme(prevActive: boolean) {
   document
-    .querySelector("body")
-    ?.setAttribute("data-theme", !prevActive ? "dark" : "light");
+    .querySelector('body')
+    ?.setAttribute('data-theme', !prevActive ? 'dark' : 'light')
 
-  const svgs: HTMLCollectionOf<Element> =
-    document.getElementsByClassName("svg");
+  const svgs: HTMLCollectionOf<Element> = document.getElementsByClassName('svg')
   for (let i = 0; i < svgs.length; i++) {
-    svgs[i].setAttribute("data-theme-svg", !prevActive ? "dark" : "light");
+    svgs[i].setAttribute('data-theme-svg', !prevActive ? 'dark' : 'light')
+  }
+
+  const inputs = document.getElementsByTagName('input')
+  if (!prevActive) {
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].style.color = 'white'
+    }
+    document.getElementsByTagName('textarea')[0].style.color = 'white'
+  } else {
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].style.color = 'black'
+    }
+    document.getElementsByTagName('textarea')[0].style.color = 'black'
   }
 }
 
 const Toggle = ({ styles }: { styles?: CSSProperties }) => {
-  const [isActive, setActive] = useState<boolean>(false);
-  const boxRef = useRef<HTMLDivElement>(null);
-  const theamModes = ["Dark Mode", "Light Mode"];
+  const [isActive, setActive] = useState<boolean>(false)
+  const boxRef = useRef<HTMLDivElement>(null)
+  const theamModes = ['Dark Mode', 'Light Mode']
   const toggleActive = () => {
     setActive((prevActive) => {
-      handleClick(boxRef, !prevActive);
-      handleTheme(prevActive);
-      return !isActive;
-    });
-  };
+      handleClick(boxRef, !prevActive)
+      handleTheme(prevActive)
+      return !isActive
+    })
+  }
   return (
     <div className={style.toggle} style={styles}>
       <div className={style.toggleButton} onClick={toggleActive}>
@@ -41,7 +53,7 @@ const Toggle = ({ styles }: { styles?: CSSProperties }) => {
       </div>
       <p>{isActive ? theamModes[0] : theamModes[1]}</p>
     </div>
-  );
-};
+  )
+}
 
-export default Toggle;
+export default Toggle
