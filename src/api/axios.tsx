1eae76c4288a2d2a29e9462acc184152519c52e1
Abdullah Khalil <abdullah.khalil123321@gmail.com>
2024-08-18 10:39:37 -0700

    env

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-// Access the environment variable with REACT_APP_ prefix
 const baseURL = process.env.NEXT_PUBLIC_API_URL
 if (!baseURL) {
   console.error('Environment variable is not defined')
