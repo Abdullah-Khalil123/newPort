5d0e55719b686ab921f729e46a0453ce05d1ceec
Abdullah Khalil <abdullah.khalil123321@gmail.com>
2024-08-24 18:49:04 +0500

    test

--- src/assets/Toggle/index.tsx
+++ src/assets/Toggle/index.tsx
@@ -17,7 +17,16 @@ const Toggle = ({ styles }: { styles?: CSSProperties }) => {
     })
   }
 
-  useEffect(() => {})
+  useEffect(() => {
+    // Check the user's system preference
+    const userPrefersDark =
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+
+    handleTheme(!userPrefersDark)
+    handleClick(boxRef, userPrefersDark)
+    setActive(userPrefersDark)
+  }, [])
   return (
     <div className={style.toggle} style={styles} onClick={toggleActive}>
       <div className={style.toggleButton}>
